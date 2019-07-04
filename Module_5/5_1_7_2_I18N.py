42a2485341a88aafb59c318bf983b984a09aedd1
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-05 00:49:01 +0200

    update i18n

---
+++ Module_5/5_1_7_2_I18N.py
@@ -0,0 +1,6 @@
+# 5.1.7.2 Characters and Strings vs. Computers
+# I18N
+# Convert name into I18N style
+
+a = input("I18N = internationalization!\nName pls:" ).upper()
+print (a[0]+str((len(a)-2))+a[-1])
