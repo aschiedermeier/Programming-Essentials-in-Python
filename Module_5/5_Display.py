9eca608e91894fd008e1b73a4013d560e87de6e9
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-05 06:13:05 +0200

    update display

---
+++ Module_5/5_Display.py
@@ -0,0 +1,39 @@
+# Number Display
+
+disp = {0:["***","* *","* *","* *","***"],
+1:["  *","  *","  *","  *","  *"],
+2:["***","  *","***","*  ","***"],
+3:["***","  *","***","  *","***"],
+4:["* *","* *","***","  *","  *"],
+5:["***","*  ","***","  *","***"],
+6:["***","*  ","***","* *","***"],
+7:["***","  *","  *","  *","  *"],
+8:["***","* *","***","* *","***"],
+9:["***","* *","***","  *","  *"]}
+
+# for j in range(10):
+#     for i in range (len(disp[0])):
+#         print (disp[j][i])
+#     print ()
+
+# for i in range (len(disp[0])): 
+#     for j in range(10):
+#         print (disp[j][i],end="")
+
+    
+print (disp[0][0],end=" ")
+print (disp[1][0],end=" ")
+print ()
+print (disp[0][1],end=" ")
+print (disp[1][1],end=" ")
+print ()
+print (disp[0][2],end=" ")
+print (disp[1][2],end=" ")
+print ()
+print (disp[0][3],end=" ")
+print (disp[1][3],end=" ")
+print ()
+print (disp[0][4],end=" ")
+print (disp[1][4],end=" ")
+print ()
+
