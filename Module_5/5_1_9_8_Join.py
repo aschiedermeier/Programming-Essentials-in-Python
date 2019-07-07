1279c97678b1afcda542c01f217a4469497b0de6
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-08 00:38:21 +0200

    update mod 5 join lower strip

--- Module_5/5_1_9_8_Join.py
+++
@@ -1,5 +0,0 @@
-# 5.1.9.8 String methods
-# join() method
-
-# Demonstrating the join() method
-print(",".join(["omicron", "pi", "rho"]))
---
+++ Module_5/5_1_9_8_JoinStrip.py
@@ -0,0 +1,19 @@
+# 5.1.9.8 String methods
+# join() method
+
+# Demonstrating the join() method
+print(",".join(["omicron", "pi", "rho"]))
+
+# Demonstrating the lower() method
+print("SiGmA=60".lower())
+
+# Demonstrating the lstrip() method
+print("[" + " tau ".lstrip() + "]")
+
+# Demonstrating the lstrip() method
+print("[" + " tau ".lstrip() + "]")
+
+print("www.cisco.com".lstrip("w.")) # takes away all leading "w" and then leading "."
+
+print("pythoninstitute.org".lstrip(".org")) # takes away nothing, as "org" is not leading
+
