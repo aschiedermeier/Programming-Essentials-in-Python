d964fc8d3e7e1cee5c21e58f4e0828d10879f89e
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-08 15:26:32 +0200

    finished mySplit
    - returns empty list for empty strings
    - strips multiple whitespaces in the middle of string to single whitespace

--- Module_5/5_1_9_18._mySplit.py
+++ Module_5/5_1_9_18._mySplit.py
@@ -13,42 +13,27 @@
 
 def mysplit(strng):
 
-    words = []
     strng = strng.strip() # strip witespace, otherwise i return every whitespace
-    start = 0
-    if strng == "":
+    while strng.find("  ") != -1: # look for double whitespaces
+        strng = strng.replace("  "," ")# replace double ws with single ws
+        
+    words = [] # create list for result
+    if strng == "": # look for empty string and return empty list 
         return words
-    while True:
+    start = 0  # start of very first word
+    while True: # infinite look, will stop when we don't find any whitespace any more
         wspace = strng.find(" ",start)
-        # print (wspace)
-        if wspace == -1:
-            word = strng[start:]
+        if wspace == -1: # no wspace any more means we reached the final word
+            word = strng[start:] # final word in string
             words.append(word)
             return words
-        word = strng[start:wspace]
-        # print (word)
-        words.append(word) 
-        start = wspace+1
+        word = strng[start:wspace] # word between start and whitespace
+        words.append(word) # add word to list
+        start = wspace+1 # start of next word
     
     return words
 
-        
-    """
-    sp = strng.find(" ") # find first whitespace
-    if sp == -1 and strng != "":
-        words.append(strng) 
-    wordstart = 0
-    while sp != -1:
-        word = strng[wordstart:sp] 
-        wordstart = sp+1
-        words.append(word) 
-        sp = strng.find(" ", wordstart)
-
-    return words
-    """    
 print(mysplit("012 456 890"))
-
-
 print(mysplit("Hi"))
 print(mysplit("Hello my    friend"))
 print(mysplit("To be or not to be, that is the question"))
