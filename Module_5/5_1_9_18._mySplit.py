7903d0b34a599c19b0bfbc986ed814572fda9c0e
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-08 02:51:52 +0200

    update my split

--- Module_5/5_1_9_18._mySplit.py
+++ Module_5/5_1_9_18._mySplit.py
@@ -6,11 +6,34 @@
 # if the string is empty, the function should return an empty list;
 # its name should be mysplit()
 
-def mysplit(strng):
-#
-# put your code here
-#
 
+# debug:
+# how to include last word: if sp != -1: just add last word
+# what to do with several spaces
+
+def mysplit(strng):
+
+    words = []
+    strng = strng.strip() # strip witespace, otherwise i return every whitespace
+    sp = strng.find(" ") # find first whitespace
+    if sp == -1 and strng != "":
+        words.append(strng) 
+    wordstart = 0
+    while sp != -1:
+        word = strng[wordstart:sp] 
+        #if word == "":
+        #    continue
+        wordstart = sp+1
+        words.append(word) 
+        sp = strng.find(" ", wordstart)
+
+    return words
+        
+
+
+
+#print(mysplit("Hi"))
+print(mysplit("Hello my    friend"))
 print(mysplit("To be or not to be, that is the question"))
 print(mysplit("To be or not to be,that is the question"))
 print(mysplit("   "))
