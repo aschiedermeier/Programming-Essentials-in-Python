cdbb87f4687d1e2819e18009d4cf309a84b4cc3a
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-13 00:30:19 +0200

    closures

---
+++ Module_6/6.1.7.13.Closures.py
@@ -0,0 +1,35 @@
+print('''
+6.1.7.13 Generators and closures
+A brief look at closures
+closure is a technique which allows the storing of values in spite of the fact 
+that the context in which they have been created does not exist anymore.
+
+''')
+
+def outer(par):
+	loc = par
+	def inner():
+		return loc
+	return inner
+
+var = 1
+fun = outer(var) # The function returned during the outer() invocation is a closure.
+print(fun())
+
+print('''
+6.1.7.13 Generators and closures
+A brief look at closures: continued
+A closure has to be invoked in exactly the same way in which it has been declared.
+
+''')
+
+def makeclosure(par):
+	loc = par
+	def power(p):
+		return p ** loc
+	return power
+
+fsqr = makeclosure(2)
+fcub = makeclosure(3)
+for i in range(5):
+	print(i, fsqr(i), fcub(i))
