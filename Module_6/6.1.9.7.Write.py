cb41506acfb6afabec66e7563f39c33720aeb51c
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-14 00:57:20 +0200

    write

--- Module_6/6.1.9.7.Write.py
+++ Module_6/6.1.9.7.Write.py
@@ -1,10 +1,13 @@
 print('''
 6.1.9.7 Working with real files
 write()
-
+The method is named write() and it expects just one argument 
+- a string that will be transferred to an open file
+the open mode should reflect the way in which the data is transferred 
+- writing a file opened in read mode won't succeed).
 ''')
 
-from os import strerror
+from os import strerror as strerr
 
 try:
 	fo = open('newtext.txt', 'wt') # a new file (newtext.txt) is created
@@ -14,4 +17,21 @@ try:
 			fo.write(ch)
 	fo.close()
 except IOError as e:
-	print("I/O error occurred: ", strerror(e.errno))
+	print("I/O error occurred: ", strerr(e.errno))
+
+print('''
+6.1.9.8 Working with real files
+write()
+write whole lines to the text file.
+''')
+
+
+from os import strerror as strerr
+
+try:
+	fo = open('newlinetext.txt', 'wt')
+	for i in range(10):
+		fo.write("line #" + str(i+1) + "\n")
+	fo.close()
+except IOError as e:
+	print("I/O error occurred: ", strerr(e.errno))
