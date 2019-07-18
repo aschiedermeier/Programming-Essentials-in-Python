590d66b867728c86762468a139d8482577b915ff
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-18 23:51:50 +0200

    mod5 quiz

---
+++ Module_5/5_2_1_1_Module_5_Quiz.py
@@ -0,0 +1,85 @@
+# 5.2.1.1 Module 5 Quiz
+# 10 questions
+# 15 minutes
+
+# 1
+print("#1: ")
+#A namespace is
+#- a space in which names exist.
+
+# 2
+print("#2: ")
+#from math import pi
+
+# 3 
+
+print("#3: ")
+#packages can contain modules
+
+# 4
+print("#4: ")
+#an exception is raised
+
+# 5
+print("#5: ")
+#the approachable except: takes the first matching brachn into consideration when an exception occurs
+
+# 6
+print("#6: ")
+#an assertion can be used to stop the program when some data have improper values
+
+# 7
+print("#7: ")
+#a code point is
+#a number which makes up a character
+
+
+# 8
+print("#8: ")
+#isalnum() is a method
+
+# 9 
+print("#9: ")
+#"mike">"Mike": True
+
+# 10
+print("#10: ")
+#+= operator, when applied to strings performs: concatenation
+
+# 11
+print("#11: ")
+
+
+# 12
+print("#12: ")
+
+
+# 13
+print("#13: ")
+
+
+# 14
+print("#14: ")
+
+
+# 15 
+print("#15: ")
+
+
+# 16
+print("#16: ")
+
+# 17
+print("#17: ")
+
+
+# 18
+
+print("#18: ")
+
+# 19
+print("#19: ")
+
+
+# 20
+print("#20: ")
