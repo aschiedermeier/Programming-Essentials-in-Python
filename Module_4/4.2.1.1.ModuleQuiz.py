472cf56242683eb5408652feba5f81b716458bd4
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-20 04:21:26 +0200

    moduel 5 quiz

---
+++ Module_4/4.2.1.1.ModuleQuiz.py
@@ -0,0 +1,29 @@
+# 4.2.1.1 Module Quiz
+
+print("#1: def is function key word")
+
+
+print("#2: function definiton must be placed before the first invocation")
+
+print("#3: function parameter is a kind of variable accessible only inside the function")
+
+print("#4:  positional: argument passing with the order of the arguments to determine the value.")
+
+print("#5: The return keyword forces a restart: False")
+
+print("#6: None keyword designates a None value")
+
+print("#7: A variable defined outside a function may be read but not written (something more is needed to do so)")
+
+print("#8: If a list is passed into a function as an argument, deleting any of its elements inside the function using the del instruction will affect the argument")
+
+print("#9: erroneous snipped: SyntaxError, keyword 'in'")
+# def fun(in=2, out=3):
+#     return in * out
+    
+# print(fun(3))
+
+print("#10: 4")
+tup =(1,)+(1,)
+tup = tup + tup
+print(len(tup))
