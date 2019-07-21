cad4706033fd2fb2ed2581e0803509e4f6688bf9
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-21 11:52:59 +0200

    update logic

--- Module_3/3_1_3_6_Logic.py
+++ Module_3/3_1_3_6_Logic.py
@@ -4,12 +4,25 @@
 
 x = 4
 y = 1
+print (bin(x))
+print (bin(y))
 
-a = x & y
+a = x & y 
 b = x | y
-c = ~x
+c = ~x # -x-1
 d = x ^ 5
 e = x >> 2
 f = x << 2
 
 print(a, b, c, d, e, f)
+
+x = 1
+y = 0
+
+z = ((x == y) and (x == y)) or not(x == y)
+print("x == y: ", x == y)
+print("(x == y) and (x == y): ", (x == y) and (x == y))
+print("not(x == y): ", not(x == y))
+print("False or True: ", False or True)
+print("z: ",z)
+print("not(z): ",not(z))
