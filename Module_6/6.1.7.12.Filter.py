324c0f7c0469930254f41a9a9f7d1086dcf7766a
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-28 15:35:01 +0200

    filter

--- Module_6/6.1.7.12.Filter.py
+++ Module_6/6.1.7.12.Filter.py
@@ -13,3 +13,8 @@ data = [ randint(-10,10) for x in range(5) ]
 filtered = list(filter(lambda x: x > 0 and x % 2 == 0, data))
 print(data)
 print(filtered)
+
+## my own filter
+
+filtered = list(filter(lambda x: x > 2,[x for x in range(5)]))
+print(filtered)
